fa57447cd3883985453f2c11ab6131d7cb4160c8
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-09-10 02:33:00 +0530

    updtaes

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Homepage from './components/Homepage';
+
+function App() {
+  return (
+    <div className="App">
+      Hello Rahul!
+      <Routes>
+        <Route path='/' element= {<Homepage/>} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/Homepage.jsx
@@ -0,0 +1,17 @@
+function homepage(){
+    return (
+        <div>
+            <h1>this is h1</h1>
+            {/* <div style= "display:flex;">
+                <div className="left">
+                    left
+                </div>
+                <div className="right" style = "color:red;">
+                    right
+                </div>
+            </div> */}
+        </div>
+    )
+}
+
+export {Homepage};
