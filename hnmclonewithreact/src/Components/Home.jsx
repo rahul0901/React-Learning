d1932f5e400b677115b1a395bc30526afaf09e97
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-09-28 21:06:56 +0530

    add

--- hnmclonewithreact/src/Components/Home.jsx
+++ hnmclonewithreact/src/Components/Home.jsx
@@ -1,7 +1,7 @@
 import './Home.css';
 const Home = (props) => {
 
-    const { HomeFirstUl, HomeBCButton } = props;
+    const { HomeFirstUl, HomeBCButton, SmallBannerBtn, PopularTitle, PopularContent, PopularImage } = props;
 
     return (
         <>
@@ -39,6 +39,40 @@ const Home = (props) => {
                             {HomeBCButton.map((content) => (
                                 <button>{content}</button>
                             ))}
+                        </div>
+                    </div>
+
+                    <div id="body-content-3">
+                        <h3>A/W23 WARDROBE WINS</h3>
+                        <p>Key staples for the season ahead!</p>
+                        {SmallBannerBtn.map((innerbtns) => (
+                            <a href="#">{innerbtns}</a>
+                        ))}
+                    </div>
+
+                    <div id="body-content-4">
+                        <h3>Popular categories</h3>
+                        <div className="bc-4-category-parent">
+                            {/* {PopularTitle.map((item) => (
+                                <div>
+                                    <h4>{item}</h4>
+                                    {PopularContent.map((item1) => (
+                                        <p>{item1}</p>
+                                    ))}
+                                </div>
+                            ))} */}
+                            {/* idhr niche kya hua 
+                                popular title ka har ek item aur uska index call kre 1st line mai
+                                aur key index ko daala taaki 8 div create ho as per apna popular title array ka hisab se.. fr har ek div mai sbse phele img aayega fr h4 aayega jisme item mtlb poputitle ka items ek ek krke ayega fr.. p tag mai Popular content bhi ek array hi hai toh PopularContent[index] me na ek ek index vakue ayega 0 se 7 taak aur usse hi PopularContent ka bhi ek ek value apneko milte rahega... fuck yeh concept sahi hai jugaad hai..:)
+                            */}
+                            {PopularTitle.map((item, index) => (
+                                <div key={index}>
+                                    <img src={PopularImage[index]} alt={`Image ${index} blank`} />
+                                    <h4>{item}</h4>
+                                    <p>{PopularContent[index]}</p>
+                                </div>
+                            ))}
+
                         </div>
                     </div>
                 </div>
