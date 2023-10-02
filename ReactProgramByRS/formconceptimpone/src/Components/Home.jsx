cfa726ba14dff8a90fff6983eaff93f705c95e57
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-03 03:07:58 +0530

    add

---
+++ ReactProgramByRS/formconceptimpone/src/Components/Home.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+
+const Home = () => {
+    const [userData, setData] = useState({ userkaname: "", userkaemail: "", userkapassword: "" })
+    console.log(userData, 'userData')
+
+    const handlechange = (event) => {
+        // console.log(event.target.name, ": ", event.target.value)
+        setData({ ...userData, [event.target.name]: event.target.value })
+    }
+
+    const Submit = (event) => {
+        event.preventDefault();
+        if (userData.userkaname && userData.userkaemail && userData.userkapassword) {
+            if(userData.userkapassword.length>=6){
+                alert('data submitted')
+            }
+            else{
+                alert('password should be long')
+            }
+        }
+        else {
+            alert('fill all fields')
+        }
+    }
+
+    return (
+        <>
+            <div>Home</div>
+            <form onSubmit={Submit}>
+                <label>Username: </label> <br />
+                <input type="text" onChange={handlechange} name='userkaname' /> <br />
+                <label>Useremail: </label> <br />
+                <input type="email" onChange={handlechange} name='userkaemail' /> <br />
+                <label>Userpassword: </label> <br />
+                <input type="password" onChange={handlechange} name='userkapassword' /> <br />
+                <input type="submit" />
+            </form>
+        </>
+    )
+}
+
+export default Home;
