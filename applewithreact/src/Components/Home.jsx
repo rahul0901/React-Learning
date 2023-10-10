8abfc4e52ac0d61533ba4276576e65f9739bab03
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-10 14:03:02 +0530

    adddd

--- applewithreact/src/Components/Home.jsx
+++ applewithreact/src/Components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css'
+// import repeatwrap from '.'
 
 const Home = () => {
   return (
@@ -37,6 +38,57 @@ const Home = () => {
             <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
             <h3>Smarter. Brighter. Mightier.</h3>
             <div className="bc-3-two-cta">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+        </div>
+
+        <div className="body-content-4">
+          <div className="bc-4-text-1">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-1">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+          <div className="bc-4-text-2">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-2">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+          <div className="bc-4-text-3">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-3">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+          <div className="bc-4-text-4">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-4">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+          <div className="bc-4-text-5">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-5">
+              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+            </div>
+          </div>
+          <div className="bc-4-text-6">
+            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+            <h3>Smarter. Brighter. Mightier.</h3>
+            <div className="bc-4-two-cta-6">
               <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
               <a>Buy <i class="fa-solid fa-greater-than"></i></a>
             </div>
