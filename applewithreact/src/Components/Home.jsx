2341ef6d90b4dd7f7859e60c1d3349be1f65c77e
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-10 14:31:45 +0530

    adddddddddd

--- applewithreact/src/Components/Home.jsx
+++ applewithreact/src/Components/Home.jsx
@@ -45,52 +45,64 @@ const Home = () => {
         </div>
 
         <div className="body-content-4">
-          <div className="bc-4-text-1">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-1">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-41">
+            <div className="bc-4-text-1">
+              <h2>iPad Pro</h2>
+              <h3>Supercharged by <img src="https://www.apple.com/v/home/be/images/logos/ipad-pro/promo_m2_chip__enw2kz91lsuq_small_2x.png" alt="" /></h3>
+              <div className="bc-4-two-cta-1">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
-          <div className="bc-4-text-2">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-2">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-42">
+            <div className="bc-4-text-2">
+              <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+              <h3>Smarter. Brighter. Mightier.</h3>
+              <div className="bc-4-two-cta-2">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
-          <div className="bc-4-text-3">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-3">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-43">
+            <div className="bc-4-text-3">
+              <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+              <h3>Smarter. Brighter. Mightier.</h3>
+              <div className="bc-4-two-cta-3">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
-          <div className="bc-4-text-4">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-4">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-44">
+            <div className="bc-4-text-4">
+              <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+              <h3>Smarter. Brighter. Mightier.</h3>
+              <div className="bc-4-two-cta-4">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
-          <div className="bc-4-text-5">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-5">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-45">
+            <div className="bc-4-text-5">
+              <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+              <h3>Smarter. Brighter. Mightier.</h3>
+              <div className="bc-4-two-cta-5">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
-          <div className="bc-4-text-6">
-            <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
-            <h3>Smarter. Brighter. Mightier.</h3>
-            <div className="bc-4-two-cta-6">
-              <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
-              <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+          <div className="bc-46">
+            <div className="bc-4-text-6">
+              <h2><img src="https://www.apple.com/v/home/be/images/logos/apple-watch-series-9/hero_logo_apple_watch_series_9__eg5xcrxghuaa_small_2x.png" alt="" /></h2>
+              <h3>Smarter. Brighter. Mightier.</h3>
+              <div className="bc-4-two-cta-6">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
             </div>
           </div>
         </div>
