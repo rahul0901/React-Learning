750686d5cd40988a3c9c001aa6ea460a38ded1bc
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-10 12:53:57 +0530

    add

--- applewithreact/src/Components/Home.jsx
+++ applewithreact/src/Components/Home.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React from 'react';
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+      <div className="home-screen">
+        <div className="offer-section">
+          <span>Get up to ₹8000.00 instant savings on selected products with eligible HDFC Bank cards.*</span> 
+          <a>Shop now</a>
+          <i class="fa-solid fa-greater-than"></i>
+        </div>
+        <div className="body-content-1">
+          <div className="bc-1-text">
+              <h2>iPhone 15 Pro</h2>
+              <h3>Titanium. So strong. So light. So Pro.</h3>
+              <div className="bc-1-two-cta">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
+          </div>
+        </div>
+
+        <div className="body-content-2">
+          <div className="bc-2-text">
+              <h2>iPhone 15 Pro</h2>
+              <h3>New Camera. New Design. Newphoria.</h3>
+              <div className="bc-2-two-cta">
+                <a>Learn more <i class="fa-solid fa-greater-than"></i></a>
+                <a>Buy <i class="fa-solid fa-greater-than"></i></a>
+              </div>
+          </div>
+        </div>
+      </div>
+    </>
   )
 }
 
