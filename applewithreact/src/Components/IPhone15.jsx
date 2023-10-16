f1080c731308fdb577ce0c200114e626b7e39ecc
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-16 21:37:43 +0530

    uppp

---
+++ applewithreact/src/Components/IPhone15.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import './IPhone15.css';
+
+const IPhone15 = () => {
+    return (
+        <>
+            <div className="screen-iphone15">
+                <div className="hdfc-offer">
+                    <span>Save up to ₹6000.00 instantly on iPhone with HDFC Bank Credit Cards. Plus No Cost EMI from most leading banks. ‡ </span>
+                    <a>Shop iPhone</a>
+                    <i class="fa-solid fa-greater-than"></i>
+                </div>
+                <div className="iphone15-content-1">
+                    <div className="ic-1-up">
+                        up
+                    </div>
+                    <div className="ic-1-down">
+                        <div className="ic-1-down-1">
+                            <h2>iPhone 15 Pro</h2>
+                        </div>
+                        <div className="ic-1-down-2">
+                            <button>Buy</button>
+                            <p>From ₹21483.00/mo.‡  or ₹134900.00</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default IPhone15;
