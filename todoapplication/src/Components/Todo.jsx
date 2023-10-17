7e8053c7970ca43f0c5eca5516ce0b0ee3c51e80
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-17 13:41:07 +0530

    pull

---
+++ todoapplication/src/Components/Todo.jsx
@@ -0,0 +1,12 @@
+function Todo(){
+    return(
+        <>
+            <div>Todo</div>
+            <form>
+                <label></label>
+            </form>
+        </>
+    )
+}
+
+export default Todo;
