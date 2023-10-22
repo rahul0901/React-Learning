1bd133206e43df3def744cbb7ff1657b0b090765
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-22 17:26:33 +0530

    pushh

---
+++ applewithreact/src/Components/AppleID.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import './AppleID.css';
+import './MediaAppleID.css';
+
+const AppleID = () => {
+    return (
+        <>
+            <div className="screen-apple-id">
+                <div className="apple-id-header">
+                    <div className="apple-id-header-1">
+                        <span>Apple ID</span>
+                    </div>
+                    <div className="apple-id-header-2">
+                        <div className="apple-id-header-2-plan-a">
+                            <a>Sign In</a>
+                            <a>Create Your Apple ID</a>
+                            <a>FAQ</a>
+                        </div>
+                        <div className="apple-id-header-2-plan-b">
+                            <a>V</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default AppleID;
