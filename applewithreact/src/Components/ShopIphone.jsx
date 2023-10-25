fe676fa15a490189d77b60bfb0281f44dc2e0064
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-10-26 01:24:45 +0530

    addddddd

--- applewithreact/src/Components/ShopIphone.jsx
+++ applewithreact/src/Components/ShopIphone.jsx
@@ -1,11 +1,20 @@
 import { useState } from 'react';
 import './ShopIphone.css';
-import { useNavigate } from 'react-router-dom';
+import ProductPopUp from './ProductPopUp';
 
 const ShopIphone = () => {
-    const route = useNavigate();
 
     const [allProducts, setProducts] = useState([{ title: "iPhone15 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhoneSE", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhoneSE", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhoneSE", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone15", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }, { title: "iPhone14 pro", image: "https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-card-40-iphone15prohero-202309?wid=680&hei=528&fmt=p-jpg&qlt=95&.v=1693086290312", description: "From ₹21483.00/mo.Per Month with instant savings§§ and No Cost EMI§ or ₹134900.00‡" }])
+
+    const [selectedProduct, setSelectedProduct] = useState(null);
+
+    const openPopup = (product) => {
+        setSelectedProduct(product);
+    };
+
+    const closePopup = () => {
+        setSelectedProduct(null);
+    };
 
     console.log(allProducts[1])
 
@@ -17,17 +26,20 @@ const ShopIphone = () => {
                 </div>
                 <div className="sis-bc-2">
                     {allProducts?.length ? <div className='sis-bc-2-allproducts'>{allProducts.map((item, index) => (
-                        <div key={index} className='sis-bc-2-single-products' onClick={()=>route(`buy-iphone/${allProducts[item]}`)}>
+                        <div key={index} className='sis-bc-2-single-products' onClick={()=> openPopup(item)}>
                             <h4>{item.title}</h4>
                             <img src={item.image} alt="" />
                             <div className="sis-bc-2-single-products-descrp">
                                 <p>{item.description}</p>
-                                <button>Buy</button>
+                                {/* <button onClick={()=> openPopup(item)}>Buy</button> */}
                             </div>
                         </div>
                     ))}</div> : <div>Loading</div>}
                 </div>
             </div>
+            {selectedProduct && (
+                <ProductPopUp product={selectedProduct} onClose={closePopup} />
+            )}
         </>
     )
 }
