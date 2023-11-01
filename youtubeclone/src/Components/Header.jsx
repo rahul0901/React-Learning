a03892ffd8fc744a5ca7a83cee50913c60863abd
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-02 00:01:39 +0530

    pushh

---
+++ youtubeclone/src/Components/Header.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Header.css';
+import './HeaderMedia.css'
+
+const Header = () => {
+    return (
+        <>
+            <div className="header-screen">
+                <div className="header-parent">
+                    <div className="header-child">
+                        <div className="header-logo">
+                            <div className="header-menubar">
+                                <i class="fa-solid fa-bars"></i>
+                            </div>
+                            <div className="header-youtube-logo"></div>
+                        </div>
+                        <div className="header-searchbox">
+                            <div className="header-search">
+                                <input type="text" placeholder='Search' />
+                                <i class="fa-solid fa-magnifying-glass"></i>
+                            </div>
+                            <div className="header-voice">
+                                <i class="fa-solid fa-microphone"></i>
+                            </div>
+                        </div>
+                        <div className="header-profile">
+                            <i class="fa-solid fa-magnifying-glass"></i>
+                            <i class="fa-brands fa-chromecast"></i>
+                            <i class="fa-regular fa-bell"></i>
+                            <i class="fa-solid fa-user"></i>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Header;
