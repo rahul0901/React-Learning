9f671cbc65bc1249f39b27899f8ed5bbc507db01
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-06 01:28:11 +0530

    pusshh

---
+++ youtubeclone/src/Components/SideHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './SideHeader.css';
+
+const SideHeader = () => {
+    return (
+        <>
+            <div className="side-header-parent">
+                <div className="side-header-child">
+
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default SideHeader;
