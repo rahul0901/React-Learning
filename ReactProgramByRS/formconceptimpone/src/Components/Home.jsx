7fe9a476a0ceeb9f9111cbb15d3412ed05a12cde
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-09 23:55:54 +0530

    push

--- ReactProgramByRS/formconceptimpone/src/Components/Home.jsx
+++ ReactProgramByRS/formconceptimpone/src/Components/Home.jsx
@@ -1,9 +1,10 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 const Home = () => {
 
-    const route = useNavigate()
+    const route = useNavigate();
 
     const [userData, setData] = useState({ userkaname: "", userkaemail: "", userkapassword: "" })
     console.log(userData, 'userData')
@@ -13,15 +14,19 @@ const Home = () => {
         setData({ ...userData, [event.target.name]: event.target.value })
     }
 
-    const Submit = (event) => {
+    const Submit = async (event) => {
         event.preventDefault();
         if (userData.userkaname && userData.userkaemail && userData.userkapassword) {
-            if(userData.userkapassword.length>=6){
-                alert('data submitted')
-                setData({ userkaname: "", userkaemail: "", userkapassword: "" })
-                route('/')
+            if (userData.userkapassword.length >= 6) {
+                const response = await axios.post("http://localhost:8000/api/v1/auth/register", { userData });
+
+                if (response.data.success) {
+                    alert('data submitted')
+                    setData({ userkaname: "", userkaemail: "", userkapassword: "" })
+                    route('/')
+                }
             }
-            else{
+            else {
                 alert('password should be long')
             }
         }
