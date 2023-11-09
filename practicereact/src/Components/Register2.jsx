faee59532dd19c53cb9defb6d878fcfd926d5931
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-09 23:56:17 +0530

    pushhh

--- practicereact/src/Components/Register2.jsx
+++ practicereact/src/Components/Register2.jsx
@@ -4,18 +4,27 @@ const Register2 = () => {
 
     const [userData, setuserData] = useState({ userKaName: "", userKaEmail: "", userKaPassword: "" })
 
+    // const router = useNavigate();
+
     console.log(userData, "userData")
+    // const router = useNavigate();
 
     const handleChange = (event) => {
         // console.log(event.target.name, ": ", event.target.value)
         setuserData({ ...userData, [event.target.name]: event.target.value })
     }
 
-    const sendDatatoBackend = (event) => {
+    const sendDatatoBackend = async (event) => {
         event.preventDefault();
         if (userData.userKaName && userData.userKaEmail && userData.userKaPassword) {
             if (userData.userKaPassword.length > 6) {
-                const response = axios.post('http://localhost:8000/register', {userData}); 
+                const response = await axios.post('http://localhost:8000/api/v1/auth/register', {userData}); 
+
+                if(response.data.success){
+                    alert('Registration Successfull')
+                    setuserData({ userKaName: "", userKaEmail: "", userKaPassword: "" })
+                    // router('/')
+                }
                 // alert(" congratulations.. ye gaya 7 crore apke bank mai")
 
             }
