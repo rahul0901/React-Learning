4a5ad65d609164b8ce6d8c0efc6da3d3bff32e0f
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-12 01:47:13 +0530

    adddddd

--- applewithreact/applebackend/Controllers/auth.controllers.js
+++ applewithreact/applebackend/Controllers/auth.controllers.js
@@ -1,7 +1,41 @@
+import UserModal from "../Modals/User.Modal.js";
+
 export const Login = (req, res) => {
     res.send('Login here')
 }
 
-export const Register = (req, res) => {
-    res.send('Register here')
+export const Register = async (req, res) => {
+    try {
+        // res.send('hii register')
+        // step1: register krne ko ky details apan lemge user se woh sab idhr aayega..req.body se lenge agar frontend connet hua toh udhr ka object data call krenge 
+        // ** imp is yaha ke variable ka name aur frontend ke variable ka same hon ahi chaiye..
+
+        const { userKaName, userKaEmail, userKaPassword } = req.body.userData;
+
+        // agar ye 3 mai se ek bhi cheej mising aaya toh backend mai all field ka error dena hai 
+        // ye if state ye bolra hi ki agar username nai hai aur 3no mai ek bhi nai hai toh yeh false return krega..
+
+        if (!userKaName || !userKaEmail || !userKaPassword) return res.status(401).json({ success: false, message: 'all field required' })
+
+        // saab frontend se data yaha taak le lega code apna abhi woh data woh database mongodb mai store krenge apan..
+        // uske liye wapis schema ko call krenge..
+        // yaha pe key joh hai woh usermodal ka schema se ayega aur value joh hai woh apna upa joh varaible define kiya hai woh jayegaa..
+
+        const user = new UserModal({
+            name: userKaName,
+            email: userKaEmail,
+            password: userKaPassword,
+        })
+
+        // abhi data aur ye schema ki madat se mongo mai save krenge data ko..
+
+        await user.save();
+
+        return res.status(200).json({ success: true, message: "Registration Successfull" })
+
+
+    } catch (error) {
+        console.log(error(error))
+        res.status(500).json({ success: false, message: "overall register error" })
+    }
 }
--- applewithreact/applebackend/index.js
+++ applewithreact/applebackend/index.js
@@ -3,13 +3,22 @@
 import express from 'express';
 import router from './Routes/index.js';
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import morgan from 'morgan';
 
 const app = express();
+dotenv.config();
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.json());
 
-app.get('/', function(req, res){
-    res.send('hi from backend')
-})
+// app.get('/', function(req, res){
+//     res.send('hi from backend')
+// })
 
 app.use('/api/v1', router)
 
-app.listen(8000, ()=> console.log('port running on 8000'))
+mongoose.connect(process.env.MongoUrl).then(()=> console.log('Database Connected'))
+
+app.listen(8000,()=> console.log('port running on 8000'))
