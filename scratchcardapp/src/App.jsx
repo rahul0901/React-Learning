4cf0faab46e3edfb8617ff33103ebc05d1a9cbb6
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-16 12:12:24 +0530

    adddddddd

---
+++ scratchcardapp/src/App.jsx
@@ -0,0 +1,14 @@
+import {Routes, Route} from 'react-router-dom'
+import Home from './Components/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
