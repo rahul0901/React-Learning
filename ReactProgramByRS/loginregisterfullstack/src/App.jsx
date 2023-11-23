ff1259bf9c28aeacb1fd4550a16563f327877c11
Rahul Shinde <82260071+rahul0901@users.noreply.github.com>
2023-11-23 21:01:48 +0530

    adddd

--- ReactProgramByRS/loginregisterfullstack/src/App.jsx
+++ ReactProgramByRS/loginregisterfullstack/src/App.jsx
@@ -1,9 +1,12 @@
 import { Routes, Route } from "react-router-dom";
+import Register from "./Components/Register";
+import Home from "./Components/Home";
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route />
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />}  />
       </Routes>
     </div>
   );
